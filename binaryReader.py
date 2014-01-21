ba7b493f3b108ff9666964697100f485138918a6
Wiktor Wolak <wwolak@wwolak.pl>
2014-01-21 11:01:01 +0100

    Add rest of function to binaryReader

--- binaryReader.py
+++ binaryReader.py
@@ -1,27 +1,50 @@
 import sys
 import argparse
+import math
 
 def tohex(s):
     return ''.join([hex(ord(i))[2:] for i in s]).upper()
-    
-    
 
 def todots(s):
     return ''.join([unichr(0x2800 + ord(i)) for i in s])
 
 def todots_pprint(s):
-    pass
+    s = todots(s)
+    l = len(s)
+    n_line = int(math.ceil(len(s)/64.))
+    lines = []
+    for i in range(n_line):
+        no = hex(i)[2:]
+        line = '0x' + '0'*(6-len(no)) + no + ' '
+        line += ' '.join([s[i*64+j*8:i*64+j*8+8] for j in range(8)])
+        lines.append(line)
+    lines.append('')
+    return '\n'.join(lines)
 
 def tohex_pprint(s):
-    pass
+    s = tohex(s)
+    l = len(s)
+    n_line = int(math.ceil(len(s)/64.))
+    lines = []
+    for i in range(n_line):
+        no = hex(i)[2:]
+        line = '0x' + '0'*(6-len(no)) + no + ' '
+        line += ' '.join([s[i*64+j*8:i*64+j*8+8] for j in range(8)])
+        lines.append(line)
+    lines.append('')
+    return '\n'.join(lines)
+
 
 def info(s):
-    pass
+    return "Length of input in bytes: %d\n" % len(s)
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-i',
+parser.add_argument('-i', '--info',
     dest='info',
+    action='store_true')
+parser.add_argument('-p', '--pprint',
+    dest='pprint',
     action='store_true')
 parser.add_argument('--hex',
     dest='view_type',
